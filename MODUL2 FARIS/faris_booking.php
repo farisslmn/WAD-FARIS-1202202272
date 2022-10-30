c8fe1f763fb167b519caed89ad503196220f00d1
farisslmn <115559173+farisslmn@users.noreply.github.com>
2022-10-30 20:57:30 +0700

    Add files via upload

---
+++ MODUL2 FARIS/faris_booking.php
@@ -0,0 +1,80 @@
+<?php
+if(isset($_GET['name'])and isset($_GET['car'])){
+    $name = $_GET['name'];
+    $car = $_GET['car'];
+
+    if($car == 'Toyota Alphard'){
+        $img = 'alphard.png';
+    }elseif($car == 'Mitsubishi Pajero Sport dakar'){
+        $img = 'pajero.jpg';
+    }elseif($car == 'Honda CR-V Turbo'){
+        $img = 'CRV.jpg';
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BookNow</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="collapse navbar-collapse justify-content-center"> >
+                <div class="navbar-nav">
+                <a class="nav-link" href="faris_home.php"> HOME <span class="sr-only"></span> </a>
+                <a class="nav-link" href="faris_booking.php">BOOKING</a>       
+                </div>        
+            </div>
+</nav>
+<header id="home" class="py-5">
+    <div class="container">
+        <h2 class="mb-4 text-center">Reservasi kendaraan EAD</h2>
+        <div class="row">
+            <div class="col-lg-6 align-self-center">
+                <?php if(isset($car)):?>
+                <img class="img-fluid" src="<?=$img?>">
+                <?php else:?>
+                <img class="img-fluid" src="alphard.jpg">
+                <?php endif?>
+            </div>
+            <div class="col-lg-6">
+                <form method="post" action="">
+                    <label for="name">Name</label>
+                    <input type="text" id="name" class="form-control mb-3" name="name" placeholder="Name" <?php if(isset($name)){echo 'value"'.$name.'"readonly';}?> require>
+                    <label for="phone">Phone Number</label>
+                    <input type="text" id="phone" class="form-control mb-3" name="phone" placeholder="Phone Number" required>
+                    <label for="date">Book Date</label>
+                    <input type="date" id="date" class="form-control mb-3" name="date"  required>
+                    <label for="time">Start Time</label>
+                    <input type="time" id="time" class="form-control mb-3" name="time"  required>
+                    <label for="Duration">Duration </label>
+                    <input type="number" id="duration" class="form-control mb-3" name="duration" placeholder="Duration"  required>
+                    <label for="email">type kendaraan</label>
+                    <?php if(isset($car)):?>
+                    <input type="text"  class="form-select mb-3" name="car" value="<?=$car?>"?>
+                    <?php else:?>
+                    <select id="mobil" class="form-select mb-3" name="car">
+                        <option value="Alphard">Alphard</option>
+                        <option value="Pajero">Pajero</option>
+                        <option value="CRV">CRV</option>
+                    </select>
+                    <?php endif?>
+                    <div class="mb-3">
+                        <div>Add Extra Service</div>
+                        <input id="supir" type="checkbox" name="supir"><label for="supir">supir / Rp.100.000</label><br>
+                        <input id="asuransi" type="checkbox" name="asuransi"><label for="asuransi">asuransi / Rp.80.000</label><br>
+                        <input id="bensin" type="checkbox" name="bensin"><label for="bensin">Full Tangki / Rp.350.000</label><br>
+                    </div>
+                    <button type="submit" class="form-control btn btn-warning">Pesan Sekarang</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ MODUL2 FARIS/faris_home.php
@@ -0,0 +1,138 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link text-light " href="faris_home.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-light" href="faris_booking.php">Booking</a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <header>
+    <div class="mb-5"></div>
+    <div class="text-center">
+      <h1>WELCOME TO EAD RENT </h1>
+      <h4>find your best deal, here!</h4>
+
+    </div>
+
+  </header>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-4">
+
+        <div class="card" style="width: 18rem;">
+          <img src="alphard.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Toyota Alphard</h5>
+            <p class="card-text">Rp2.500.000/hari </p>
+          </div>
+          <ul class="list-group list-group-flush text-center">
+            <li class="list-group-item">7 seater </li>
+            <li class="list-group-item">2.5000cc</li>
+            <li class="list-group-item">Automatic</li>
+          </ul>
+          <div class="card-body text-center">
+            <a href="faris_booking.php" class="btn btn-primary">Book Now</a>
+
+          </div>
+
+        </div>
+      </div>
+      <div class="col-md-4">
+
+        <div class="card" style="width: 18rem;">
+          <img src="pajero.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Mitsubishi Pajero Sport dakar</h5>
+            <p class="card-text">Rp1.500.000/hari </p>
+          </div>
+          <ul class="list-group list-group-flush text-center">
+            <li class="list-group-item">7 seater </li>
+            <li class="list-group-item">2.4000cc</li>
+            <li class="list-group-item">Automatic</li>
+          </ul>
+          <div class="card-body text-center">
+            <a href="faris_booking.php" class="btn btn-primary">Book Now</a>
+
+          </div>
+
+        </div>
+      </div>
+      <div class="col-md-4">
+
+        <div class="card" style="width: 18rem;">
+          <img src="CRV.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Honda CR-V Turbo</h5>
+            <p class="card-text">Rp1.300.000/hari </p>
+          </div>
+          <ul class="list-group list-group-flush text-center">
+            <li class="list-group-item">7 seater </li>
+            <li class="list-group-item">1.5000cc</li>
+            <li class="list-group-item">Automatic</li>
+          </ul>
+          <div class="card-body text-center">
+            <a href="faris_booking.php" class="btn btn-primary">Book Now</a>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+  </div>
+  </section>
+  <footer class="py-4 text-center" style="background-color: rgb(250, 245, 240)">
+    <a data-bs-toggle="modal" data-bs-target="#exampleModal">Copyright 2022 FARIS_1202202272_SI4406</a>
+  </footer>
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1>Copyright</h1>
+        </div>
+        <div class="modal-body">
+          Nama : Dhiaulhaq Salman Alfarisi<br>
+          NIM  : 1202202272<br>
+          Kelas: SI4406
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+    crossorigin="anonymous"></script>
+
+
+
+
+</body>
+
+</html>
